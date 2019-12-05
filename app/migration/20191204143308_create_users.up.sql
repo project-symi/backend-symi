f7f2b4c1f937925b02de139b416b55769c5db93f
Yukio Ueda <52945670+Yukio0315@users.noreply.github.com>
2019-12-05 13:52:03 +0900

    modified users migration

--- app/migration/20191204143308_create_users.up.sql
+++ app/migration/20191204143308_create_users.up.sql
@@ -1,11 +1,9 @@
 START TRANSACTION;
 set
   foreign_key_checks = 0;
-DROP TABLE IF EXISTS feedbacks;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS types;
 DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS categories;
 CREATE TABLE types (
     id INT AUTO_INCREMENT NOT NULL UNIQUE,
     name VARCHAR(20) UNIQUE,
@@ -37,28 +35,6 @@ CREATE TABLE users (
     modified_at TIMESTAMP,
     FOREIGN KEY (department_id) REFERENCES departments(id),
     FOREIGN KEY (type_id) REFERENCES types(id),
-    PRIMARY KEY (id)
-  );
-CREATE TABLE categories (
-    id INT AUTO_INCREMENT NOT NULL UNIQUE,
-    name VARCHAR(20) UNIQUE,
-    deleted boolean,
-    deleted_at DATETIME,
-    created_at TIMESTAMP,
-    modified_at TIMESTAMP,
-    PRIMARY KEY (id)
-  );
-CREATE TABLE feedbacks (
-    id INT AUTO_INCREMENT NOT NULL UNIQUE,
-    feedback_note VARCHAR(5000),
-    user_id INT NOT NULL,
-    category_id INT NOT NULL,
-    deleted boolean,
-    deleted_at DATETIME,
-    created_at TIMESTAMP,
-    modified_at TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id),
     PRIMARY KEY (id)
   );
 set
