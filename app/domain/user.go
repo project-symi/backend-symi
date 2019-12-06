72b0ac03cb7fa0af5c72d2ec39c2a67ce7ce014c
Yukio Ueda <52945670+Yukio0315@users.noreply.github.com>
2019-12-06 09:53:12 +0900

    added json option to the User structure

--- app/domain/user.go
+++ app/domain/user.go
@@ -1,13 +1,13 @@
 package domain
 
 type User struct {
-	EmployeeId  int
-	Name        string
-	Mail        string
-	Department  string
-	DateOfBirth string
-	Gender      string
-	Type        string
+	EmployeeId  int    `json:"employeeId"`
+	Name        string `json:"name"`
+	Mail        string `json:"mail"`
+	Department  string `json:"department"`
+	DateOfBirth string `json:"dateOfBirth"`
+	Gender      string `json:"gender"`
+	Type        string `json:"type"`
 }
 
 type Users []User
