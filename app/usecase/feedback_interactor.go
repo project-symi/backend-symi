be817eda8c6aff6c17fda1274517e2136cdbfcf4
Yukio Ueda <52945670+Yukio0315@users.noreply.github.com>
2019-12-13 23:08:34 +0900

    changed string chain

--- app/usecase/feedback_interactor.go
+++ app/usecase/feedback_interactor.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"project-symi-backend/app/domain"
+	"strings"
 )
 
 type FeedbackInteractor struct {
@@ -26,19 +27,7 @@ func (interactor *FeedbackInteractor) FindByEmployeeId(employeeId string) (feedb
 }
 
 func (interactor *FeedbackInteractor) ChangeToSeen(ids []string) (numberOfChanged int, err error) {
-	query := createIdsQuery(ids)
+	query := "(" + strings.Join(ids, ", ") + ")"
 	numberOfChanged, err = interactor.FeedbackRepository.UpdateSeen(query)
-	return
-}
-
-func createIdsQuery(ids []string) (query string) {
-	query = "("
-	for i, id := range ids {
-		if i != len(ids)-1 {
-			query += id + ", "
-		} else {
-			query += id + ")"
-		}
-	}
 	return
 }
